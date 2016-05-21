47a768729fc200b871fd57c8dc5966bcfb5f26fe
Админ <@DESKTOP-0I26FI7>
2016-05-21 14:58:05 +0300

    project_start

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title><?= $this->meta_t ?></title>
+        <meta name="description" content="<?= $this->meta_d ?>" />
+        <meta name="keywords" content="<?= $this->meta_k ?>" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link type="text/css" rel="stylesheet" href="/css/style.css" />
+        <link type="text/css" rel="stylesheet" href="/css/style.css" />
+        	
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+  
+        
+    </head>
+    
+    <body></body>
+</html>
