14dc11af5aef9f4651c8b3fdf1d414ee5e1549b5
IllyaLaba <illya.laba@gmail.com>
2016-05-21 19:39:22 +0300

    project_karkas

--- index.php
+++ index.php
@@ -2,17 +2,165 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title><?= $this->meta_t ?></title>
-        <meta name="description" content="<?= $this->meta_d ?>" />
-        <meta name="keywords" content="<?= $this->meta_k ?>" />
+        <title>123</title>
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
         <link rel="shortcut icon" href="/images/favicon.ico" />
-        <link type="text/css" rel="stylesheet" href="/css/style.css" />
-        <link type="text/css" rel="stylesheet" href="/css/style.css" />
-        	
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-  
-        
+        <link type="text/css" rel="stylesheet" href="/css/bootstrap.css" /> 
+        <link type="text/css" rel="stylesheet" href="/css/style.css" />    	
+
     </head>
-    
-    <body></body>
+
+    <body>
+
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <!--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                      <span class="sr-only">Студия ТВС</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                    </button>
+                    --> 
+                    <a class="navbar-brand" href="#">Студия ТВС</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Разработка</a></li>
+                        <li><a href="#about">Техническая поддержка</a></li>
+                        <li><a href="#contact">Продвижение и реклама</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">О нас <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Портфолио</a></li>
+                                <li><a href="#">Партнёры</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="../navbar/">Default</a></li>
+                        <li><a href="../navbar-static-top/">Static top</a></li>
+                        <li class="active"><a href="./">Контакты<span class="sr-only">(current)</span></a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3" style="height:200px; background: #f1f1f1;"></div>
+                <div class="col-md-9" style="height:200px; background: #f4f4f4;"></div>
+                
+            </div>
+        </div> 
+        <div class="development">
+        <div class="container ">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#" title="Разработка сайта">
+                        <img class="img-responsive" src="/images/icon/dev_prostoi-saite.png">
+                        <p class="text-center">Простой сайт</p>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                     <a href="#" title="Разработка сайта">
+                        <img class="img-responsive" src="/images/icon/dev_internet-magazin.png">
+                        <p class="text-center">Интернет-магазин</p>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                     <a href="#" title="Разработка сайта">
+                        <img class="img-responsive" src="/images/icon/dev_individualniy.png">
+                        <p class="text-center">Сложная разработка</p>
+                    </a>
+                </div>
+            </div>
+        </div> 
+        </div> 
+        
+        <div class="support">
+         <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#" title="Техническая поддержка">
+                        <img class="img-responsive" src="/images/icon/soprovogdenie-saita-standart.png" >
+                        <p class="text-center">Пакет "Стандартный"</p>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                     <a href="#" title="Техническая поддержка">
+                        <img class="img-responsive" src="/images/icon/soprovogdenie-saita-rasshirenniy.png" >
+                        <p class="text-center">Пакет "Расширенный"</p>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                     <a href="#" title="Техническая поддержка">
+                        <img class="img-responsive" src="/images/icon/soprovogdenie-saita-premiym.png" >
+                        <p class="text-center">Пакет "Премиум"</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+        </div>
+        
+         <div class="advertising">
+             <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#" title="Продвижение и реклама">
+                        <img class="img-responsive" src="/images/icon/Promo_Google_Adwords.png">
+                        <p class="text-center">Простой сайт</p>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                     <a href="#" title="Продвижение и реклама">
+                        <img class="img-responsive" src="/images/icon/Promo_Yandex.png">
+                        <p class="text-center">Интернет-магазин</p>
+                    </a>
+                </div>
+                
+            </div>
+        </div>
+        </div>
+        
+        
+        
+        <div class="footer" >
+            <div class="container" >
+            <div class="row" style="margin: 50px;">
+                <div class="col-md-3" style="border-right: 2px solid #000; height:100px;">
+                   
+                </div>
+                <div class="col-md-3" style="border-right: 2px solid #000; height:100px;">
+                    
+                </div>
+                
+                <div class="col-md-3" style="border-right: 2px solid #000; height:100px;">
+                    
+                </div>
+                <div class="col-md-3" style="border-right: #000; height:100px;">
+                    
+                </div>
+                
+            </div>
+        </div>
+        </div>
+        
+        <!-- /container -->
+        <!--Скрипты -->
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+        crossorigin="anonymous"></script>
+        <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+
+
+    </body>
 </html>
---
+++ index2.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Внутрення страничка</title>
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link type="text/css" rel="stylesheet" href="/css/bootstrap.css" /> 
+        <link type="text/css" rel="stylesheet" href="/css/style.css" />    	
+<link href='https://fonts.googleapis.com/css?family=Ubuntu:400,400italic,500,500italic,300,300italic,700,700italic&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700,700italic&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+    </head>
+
+    <body>
+
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <!--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                      <span class="sr-only">Студия ТВС</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                    </button>
+                    --> 
+                    <a class="navbar-brand" href="#">Студия ТВС</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Разработка</a></li>
+                        <li><a href="#about">Техническая поддержка</a></li>
+                        <li><a href="#contact">Продвижение и реклама</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">О нас <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Портфолио</a></li>
+                                <li><a href="#">Партнёры</a></li>
+                                <li><a href="#">Блог</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li class="dropdown-header">Лучшие кейсы:</li>
+                                <li><a href="#">sall.com.ua</a></li>
+                                <li><a href="#">super-prazdnik.com</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="tws-tel"><a href="tel:+79647000507">+7(964) 7000 507</a></li>
+                        <li class="active"><a href="#">Контакты<span class="sr-only">(current)</span></a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
+
+        <div class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 left-menu">
+                        <div class="list-group">
+                            <a href="#" class="list-group-item disabled">
+                                Комплексные решения
+                            </a>
+                            <a href="#" class="list-group-item">Простой сайт</a>
+                            <a href="#" class="list-group-item">Интернет-магазин</a>
+                            <a href="#" class="list-group-item">Корпоративный сайт</a>
+
+                        </div>
+                        <div class="list-group">
+                            <a href="#" class="list-group-item disabled">
+                                Дополнительно
+                            </a>
+                            <a href="#" class="list-group-item">Разработка дизайна</a>
+                            <a href="#" class="list-group-item">Вёрстка шаблонов</a>
+                            <a href="#" class="list-group-item">Разработка компонентов</a>
+
+                        </div>
+
+                    </div>
+                    <div class="col-md-9">
+                        <ol class="breadcrumb">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Library</a></li>
+                            <li class="active">Data</li>
+                        </ol>
+                        <div class="content-text">
+                            <h1>Простой сайт</h1>
+                            <img src="/images/development/site-vizitka.png">
+                            <p>Простой сайт выполнит роль визитной карточки Вашей фирмы в интернете. Он содержит лишь основную информацию: направление деятельности компании, предоставляемые товары и услуги (без каталога продукции), контакты. Такие данные обновляются 2-3 раза в год, поэтому такой сайт не требует постоянного обслуживания.</p>
+                            <h2>Вам подойдёт простой сайт (сайт-визитка), если:</h2>
+                            <ul>
+                                <li>У Вас никогда не было сайта;</li>
+                                <li>Вы хотите просто заявить о себе в интернете;</li>
+                                <li>У Вас минимальный бюджет, выделенный на создание сайта.</li>
+                                <li>Пока нет необходимости размещать большое количество информации и постоянно обновлять её;</li>
+                            </ul>
+                            <h2>Несмотря на простоту, у простого сайта есть ряд преимуществ:</h2>
+
+
+
+                            <ul>
+                                <li>Минимальная стоимость создания сайта;</li>
+                                <li>Нет необходимости в постоянном обслуживании - Вы сможете менять основную информацию самостоятельно;</li>
+                                <li>Сайт повысит узнаваемость фирмы;</li>
+                                <li>К своим контактным данным Вы сможете добавить адрес сайта (на визитках, буклетах, в объявлениях, документах)</li>
+                                <li>Короткие сроки создания сайта;</li>
+                            </ul>
+                       
+                        <h3>Стандартные разделы, которые могут входить в сайт-визитку:</h3>
+                        <ul>
+
+                            <li>Раздел «о компании»;</li>
+                            <li>Фотогалерея;</li>
+                            <li>Новости;</li>
+                            <li>Раздел FAQ (ответы на вопросы пользователей)</li>
+                            <li>Поиск по сайту;</li>
+                            <li>Прайс-лист в формате Microsoft Excel, который Вы сможете загружать самостоятельно;</li>
+                            <li>Контактные данные (адреса, телефоны, схемы проезда) и другие простые модули.</li>
+                        </ul>
+                        <p>Что касается раскрутки и продвижения сайта-визитки – тут необходимо учесть, что это возможно только в неконкурентных сферах и в отдельном регионе. Для продвижения сайта в конкурентной тематике практически сразу потребуется расширение и усложнение структуры сайта, увеличение объёма информации на нём. В таком случае сайт перестанет быть простым.</p>
+                        <p>Если Вам необходим простой сайт или у Вас остались вопросы относительно процесса его создания, выбора подходящих доменного имени и хостинга, раскрутки и продвижения – мы будем рады проконсультировать Вас более подробно по телефонам: +3(063) 687-10-98, +7(964) 700-05-07.</p>
+
+
+                    </div>
+
+                </div>
+            </div> 
+        </div> 
+
+
+        <div class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p><a role="button" href="#" class="btn btn-primary">View details »</a></p>
+                    </div>                
+
+                    <div class="col-md-4">
+                        <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p><a role="button" href="#" class="btn btn-primary">View details »</a></p>
+                    </div>
+                    <div class="col-md-4">
+                        <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p><a role="button" href="#" class="btn btn-primary">View details »</a></p>
+                    </div>
+
+                </div>
+            </div> 
+        </div> 
+
+
+
+
+
+        <div class="footer" >
+            <div class="container" >
+                <div class="row">
+                    <div class="col-md-3">         </div>
+                    <div class="col-md-3">         </div>
+
+                    <div class="col-md-3">       </div>
+                    <div class="col-md-3" >           </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!-- /container -->
+        <!--Скрипты -->
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+        crossorigin="anonymous"></script>
+        <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+
+
+    </body>
+</html>
